b195932fadc3e7619f1d245a9e433531ce401f13
Cash Map <cashmap@Daniels-MacBook-Pro.local>
2018-06-28 12:26:55 -0500

    Login page changes

--- components/Login/Login.js
+++ components/Login/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   TextInput,
   Text,
+  Image,
   View, 
   TouchableHighlight } from 'react-native';
 
@@ -9,6 +10,7 @@ import styles from './styles'
 
 const SideMenu = (props) => 
 <View style={styles.container}>
+  <Image style={styles.logo} source={require('./LOGOCASHMAP.png')} />
   <TextInput style={styles.textInput} placeholder="Username" onChangeText={props.handleChangeUsername}/>
   <TextInput style={styles.textInput} secureTextEntry={true} placeholder="Password" onChangeText={props.handleChangePassword}/>
   <Text style={styles.colorRed}>{props.errorMessage}</Text>
