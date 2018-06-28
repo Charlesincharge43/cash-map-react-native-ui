3f6a9a5c018f71bcd805bfe458d3b765f290b2b4
Anne Marie King <amking23@gmail.com>
2018-06-28 11:50:03 -0500

    POI info

--- components/Map/POIDetails/POIDetails.js
+++ components/Map/POIDetails/POIDetails.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, Text, Dimensions, StyleSheet } from 'react-native';
+import { View, Text, Dimensions, StyleSheet, Image } from 'react-native';
+
 
 import styles from './styles';
 import withAnimationVerticalSlide from '../../shared/hoc/withAnimationVerticalSlide';
@@ -8,9 +9,28 @@ const deviceHeight = Dimensions.get('window').height;
 
 const POIDetailsView = (props) =>
 <View style={styles.container}>
-  <Text>
-    POI Details Placeholder
-  </Text>
+  <Text style={styles.placeHeading}>Starbucks</Text>
+  <View style={{flexDirection: 'row'}}>
+    <View style={{flexDirection: 'column'}}>
+      <View style={{flexDirection: 'row'}}>
+        <Text style={styles.infoTextBig}>4.3</Text>
+        <Text style={styles.infoTextBigDivider}>·</Text>
+        <Text style={styles.infoTextBig}>4.3</Text>
+        <Text style={styles.infoTextBigDivider}>·</Text>
+        <Text style={styles.infoTextBig}>4.3</Text>
+      </View>
+      <View style={{flexDirection: 'row'}}>
+        <Text style={styles.infoTextSmall}>4.3</Text>
+        <Text style={styles.infoTextSmall}>4.3</Text>
+        <Text style={styles.infoTextSmall}>4.3</Text>
+      </View>
+    </View>
+  </View>
+  <View style={{flexDirection: 'row'}}>
+    <Text style={styles.percent}>5%</Text>
+    <Image source={require('./images/freedomunlimited.png')} style={styles.cardLogo} />
+  </View>
+  <Image source={require('./images/starbucks.png')} style={styles.placeImage} />
 </View>
 
 const POIDetailsAnimated = withAnimationVerticalSlide(deviceHeight, deviceHeight - StyleSheet.flatten(styles.container).height, 350)(POIDetailsView);
--- components/Map/POIDetails/styles.js
+++ components/Map/POIDetails/styles.js
@@ -4,8 +4,38 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'white',
-    height: 150,
-  }
+    height: 250,
+    padding: 14,
+  },
+  cardLogo: {
+  },
+  placeHeading: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    paddingBottom: 4,
+  },
+  percent: {
+    color: 'orange',
+    fontSize: 20,
+    fontWeight: 'bold',
+    paddingBottom: 1,
+  },
+  placeImage: {
+    width: 100,
+    height: 100,
+  },
+  infoTextBig: {
+    fontSize: 17,
+  },
+  infoTextBigDivider: {
+    fontSize: 17,
+    paddingLeft: 2,
+    paddingRight: 2,
+  },
+  infoTextSmall: {
+    color: 'grey',
+    fontSize: 15,
+  },
 })
 
 export default styles;
