897f3294f5a719de0e9582ddb4e8e449c6bc2274
Anne Marie King <amking23@gmail.com>
2018-06-28 13:41:41 -0500

    integrating POI JSON with UI

--- components/Map/POIDetails/POIDetails.js
+++ components/Map/POIDetails/POIDetails.js
@@ -8,33 +8,46 @@ import withAnimationVerticalSlide from '../../shared/hoc/withAnimationVerticalSl
 
 const deviceHeight = Dimensions.get('window').height;
 
-const POIDetailsView = (props) =>
-//props.placesOfInterest = places on map
-//click on marker - get selected index selectedPOIDetailIdx
-<View style={styles.container}>
-  <Text style={styles.placeHeading}>Starbucks</Text>
-  <View style={{flexDirection: 'row'}}>
-    <View style={{flexDirection: 'column'}}>
-      <View style={{flexDirection: 'row'}}>
-        <Text style={styles.infoTextBig}>Coffe Shop</Text>
-        <Text style={styles.infoTextBigDivider}>·</Text>
-        <Text style={styles.infoTextBig}>0.2 mi</Text>
-        <Text style={styles.infoTextBigDivider}>·</Text>
-        <Text style={styles.infoTextBig}>10AM - 9PM</Text>
+const POIDetailsView = (props) => {
+  let placeDetail;
+  if(props.selectedPOIDetailIdx){
+    placeDetail = props.placesOfInterest[props.selectedPOIDetailIdx]
+  }
+  if(placeDetail){
+    return (
+      //props.placesOfInterest = places on map
+      //click on marker - get selected index selectedPOIDetailIdx
+      <View style={styles.container}>
+        <Text style={styles.placeHeading}>{placeDetail.title}</Text>
+        <View style={{flexDirection: 'row'}}>
+          <View style={{flexDirection: 'column'}}>
+            <View style={{flexDirection: 'row'}}>
+              <Text style={styles.infoTextBig}>{placeDetail.category}</Text>
+              <Text style={styles.infoTextBigDivider}>·</Text>
+              <Text style={styles.infoTextBig}>0.2 mi</Text>
+              <Text style={styles.infoTextBigDivider}>·</Text>
+              <Text style={styles.infoTextBig}>10AM - 9PM</Text>
+            </View>
+            <View style={{flexDirection: 'row'}}>
+              <Text style={styles.infoTextSmall}>{placeDetail.additionalDetails.rating} ★★★★</Text>
+              <Text style={styles.infoTextSmallDivider}>·</Text>
+              <Text style={styles.infoTextSmall}>$$</Text>
+            </View>
+          </View>
+        </View>
+        <View style={{flexDirection: 'row'}}>
+          <Text style={styles.percent}>5%</Text>
+          <Image source={require('./images/freedomunlimited.png')} style={styles.cardLogo} />
+        </View>
+        <Image source={require('./images/starbucks.png')} style={styles.placeImage} />
       </View>
-      <View style={{flexDirection: 'row'}}>
-        <Text style={styles.infoTextSmall}>4.3 ★★★★</Text>
-        <Text style={styles.infoTextSmallDivider}>·</Text>
-        <Text style={styles.infoTextSmall}>$$</Text>
-      </View>
-    </View>
-  </View>
-  <View style={{flexDirection: 'row'}}>
-    <Text style={styles.percent}>5%</Text>
-    <Image source={require('./images/freedomunlimited.png')} style={styles.cardLogo} />
-  </View>
-  <Image source={require('./images/starbucks.png')} style={styles.placeImage} />
-</View>
+    )
+  } else {
+    return (
+      <View />
+    )
+  }
+}
 
 const POIDetailsAnimated = withAnimationVerticalSlide(deviceHeight, deviceHeight - StyleSheet.flatten(styles.container).height, 350)(POIDetailsView);
 
